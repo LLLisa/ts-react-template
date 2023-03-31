e7b91a3680070867f766c06646e9752738f7ed31
Lisa Knox <lisaknox83@gmail.com>
2023-03-31 10:59:31 -0400

    scripts scripting

--- build/server/index.js
+++ build/server/index.js
@@ -4,5 +4,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const PORT = process.env.port || 1957;
+const PORT = process.env.port || 1958;
 app_1.default.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
-const PORT = process.env.port || 1957;
+const PORT = process.env.port || 1956;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
