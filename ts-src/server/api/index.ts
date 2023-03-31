7dab75b44da05daec4ac75bad33cff14cec067aa
Lisa Knox <lisaknox83@gmail.com>
2023-03-31 12:00:04 -0400

    css connecto

--- ts-src/server/api/index.ts
+++ ts-src/server/api/index.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const app = express();
 
-// app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use('/public', express.static(path.join(__dirname, '../../../public')));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../../../public/index.html'));
