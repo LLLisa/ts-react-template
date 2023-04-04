19387e05592e63d0fb2441eb9d1695405ce2399d
Lisa Knox <lisaknox83@gmail.com>
2023-04-04 11:05:45 -0400

    deleted build dir

--- build/server/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const api_1 = __importDefault(require("./api"));
-const PORT = process.env.port || 1956;
-api_1.default.listen(PORT, () => console.log(`listening on port ${PORT}`));
-//# sourceMappingURL=index.js.map
